ba5289e9e250127506c766349dfc0882adb38dfa
Camilla De Pretto <161570720+camilla-cs@users.noreply.github.com>
2024-12-20 13:27:02 +1100

    fixed import post controller

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,4 @@
-const Post = require ("../models/PostModel.js");
+const {Post} = require ("../models/PostModel.js");
 
 // create a new post (only admin)
 const createPost= async (request, response) => {
